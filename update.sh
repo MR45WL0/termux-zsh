a21f264febb0a2e765572e4b0db78ce2d5c8e761
MR45WL0 <40099221+MR45WL0@users.noreply.github.com>
2018-07-16 04:14:52 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
+
+echo "Please restart termux!"
+
+echo "Kalo Masih Bingung Cek Yt Saya"
+
+echo "youtube.com/c/StereoBlaze"
+
+sleep 2
+
+exit
